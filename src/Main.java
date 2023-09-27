d46c9d4aeb9d032586f2765de380732a57b61c7e
dado-offbrand <82716251+dado-offbrand@users.noreply.github.com>
2023-09-27 09:32:05 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String party = "";
+
+        System.out.println("What is your party affiliation? Democrat, Republican, or Independent? (D/R/I)");
+        if (scanner.hasNextLine()) {
+            party = scanner.nextLine();
+            party = party.toLowerCase();
+
+            if (party.equals("d")) {
+                System.out.println("You get a democratic donkey!");
+            } else if (party.equals("r")) {
+                System.out.println("You get a republican elephant!");
+            } else if (party.equals("i")) {
+                System.out.println("You get an independent inguana!");
+            } else {
+                System.out.println("You get an other otter!");
+            }
+        } else {
+            System.out.println("That isnt' a proper answer.");
+            System.exit(0);
+        }
+    }
+}
